3b450b50bb77b737ac40798dae219df0bb69462c
Billy Liandy <billyliandy21@gmail.com>
2019-09-03 12:31:30 +1000

    First Commit

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 let express =require('express');
 let app = express();
-let db= [];
 let bodyParser=require('body-parser');
+// let viewPath= (__dirname+"/views/");
+var ObjectId = require('mongodb').ObjectID
+
+//set up MONGO DB
+let mongoDb=require('mongodb');
+const MongoClient=mongoDb.MongoClient;
+const url = "mongodb://localhost:27017";               
+let db= null;
+let col =null;
+
+MongoClient.connect(url,{useNewUrlParser:true},function(err,client){
+    if(!err){
+        console.log('MongoDB Connected Successfully');
+        db=client.db('fit2095')
+        // col2=db.createCollection("Tasks");
+        // coll =db.collection("Tasks");
+    }else{
+        console.log('Mongo Connection Error',err);
+    }
+})
+
 
 app.use(express.static("public/img"))
 app.use(express.static("views"))
@@ -22,23 +42,78 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/addNewTask",(req,res)=>{
+    // console.log("Homepage request")
+    // col.find({}).toArray(function(err,Week6Lab){
+    //     console.log("Find query", Week6Lab)
+    // })
     res.render("addNewTask.html");
 })
 
+//ADD TASKS
 app.post('/newTask',(req,res)=>{
-    // console.log(req.body);
-    db.push(req.body);
-    console.log(db);
-    res.send("Task Added");
-})
+    let taskDetails=req.body;
+    console.log(taskDetails); 
+    db.collection('tasks').insertOne({
+        name: taskDetails.name,
+        dueDate: taskDetails.dueDate, 
+        description:taskDetails.description,
+        assignto:taskDetails.assignto,
+        status:taskDetails.taskstatus
+    });
+    // coll.find({}).toArray(function(err,dbTasks){
+    //     console.log("Tasks in DB",dbTasks);
+    res.redirect('/listTask');
+    })
+
 
 app.get("/listTask",(req,res)=>{
-    res.render("listTask.html",{
-        tasks:db
+    db.collection('tasks').find({}).toArray(function (err,data){
+        res.render('listTask.html',{
+            tasksDb:data
+        })
     })
 
 })
 
+//Update tasks: 
+//GET request: send the page to the client 
+app.get('/updatetask', function (req, res) {
+    res.render('updatetask.html');
+});
+//POST request: receive the details from the client and do the update
+app.post('/updatetaskdata', function (req, res) {
+    let taskDetails = req.body;
+    console.log(taskDetails);
+    
+    let filter = { _id: ObjectId(taskDetails.id) };
+    let theUpdate = { $set: { 
+        status:taskDetails.taskstatusnew
+    } };
+    db.collection('tasks').updateOne(filter, theUpdate);
+    res.redirect('/listTask');// redirect the client to list users page
+})
+
+//Delete Task: 
+//GET request: send the page to the client to enter the user's name
+app.get('/deletetask', function (req, res) {
+    res.render('deletetask.html');
+});
+//POST request: receive the user's name and do the delete operation 
+app.post('/deletetaskdata', function (req, res) {
+    let taskDetails = req.body;
+    let filter = { id: taskDetails._id };
+    db.collection('tasks').deleteOne(filter);
+    res.redirect('/listTask');// redirect the client to list users page
+});
+
+app.post('/deletecompleted', function (req, res) {
+    // let taskDetails = req.body;
+    // let filter = { status: taskDetails.status };
+    db.collection('tasks').deleteMany({status: "completed"}, function (err, obj){
+        
+    });
+    res.redirect('/listTask');// redirect the client to list users page
+});
 app.listen(3000,()=>{
     console.log('Listening on 3000')
 })
